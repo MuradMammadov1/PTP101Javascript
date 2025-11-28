a37cf4bdd1c69a8ca3cb3316b48d41d32b129491
Murad S. Mammadov <muradsma@code.local>
2025-11-28 13:55:14 +0400

    Javascript

---
+++ 01.task/script.js
@@ -0,0 +1,27 @@
+// const person = {
+//     name: "Murad",
+//     age: 17
+// };
+// console.log innerHTML =
+//     `Ad: ${person.name} <br> Yaş: ${person.age}`;
+// console.log(person);
+
+
+
+
+
+
+
+
+
+
+
+// const dividend = 13;
+// const divisor = 4;
+
+// let res = dividend % divisor
+
+
+
+// console.log(res);
+
---
+++ 15.js-variables-operators.conditions/script.js
@@ -0,0 +1,107 @@
+// let number = 12;
+
+// for (let i = 1; i <= number; i++) {
+//   if (number % i === 0) {
+//     console.log(i);
+//   }
+// }
+
+
+
+
+
+
+
+// let number1 = 12;
+// let count = 0;
+
+// for (let i = 1; i <= number; i++) {
+//   if (number % i === 0) {
+//     count++;
+//   }
+// }
+
+// console.log(`${number} ədədinin ${count} böləni var`);
+
+
+
+
+
+
+
+
+
+
+// let tekSum = 0;
+// let cutSum = 0;
+
+// for (let i = 0; i <= 100; i++) {
+//   if (i % 2 === 0) {
+//     cutSum += i;
+//   } else {
+//     tekSum += i;
+//   }
+// }
+
+// console.log("Tək ədədlərin cəmi:", tekSum);
+// console.log("Cüt ədədlərin cəmi:", cutSum);
+
+
+
+
+
+
+
+
+
+
+// const numbers2 = [3, 7, 12, 20, 25];
+// let input = 12;
+
+// let found = false;
+
+// for (let num of numbers) {
+//   if (num === input) {
+//     found = true;
+//     break;
+//   }
+// }
+
+// if (found) {
+//   console.log(`${input} array daxilində mövcuddur`);
+// } else {
+//   console.log(`${input} array daxilində yoxdur`);
+// }
+
+
+
+
+
+
+
+
+
+
+// const numbers3 = [4, 2, 7, 9, 3, 12, 46, 32, 19];
+
+// let min = numbers[0];
+// let max = numbers[0];
+
+// for (let num of numbers) {
+//   if (num < min) min = num;
+//   if (num > max) max = num;
+// }
+
+// console.log("Minimum:", min);
+// console.log("Maximum:", max);
+
+
+
+
+
+
+
+
+
+
+
