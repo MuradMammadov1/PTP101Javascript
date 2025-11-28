e5e6e9a3a85d60b1933f6c78225f38ab6069530f
Murad S. Mammadov <muradsma@code.local>
2025-11-28 14:53:16 +0400

    16.js

---
+++ 16.jsvariables-operators.conditions/script.js
@@ -0,0 +1,88 @@
+// 1//
+// function sumBetween(a, b) {
+//     let small = Math.min(a, b);
+//     let big = Math.max(a, b);
+//     let sum = 0;
+
+//     for (let i = small; i <= big; i++) {
+//         sum += i;
+//     }
+
+//     return sum;
+// }
+
+// console.log(sumBetween(3, 8));  
+
+
+
+
+
+
+
+
+// 2//
+// let sentence = "I love JavaScript and coding";
+
+// let words = sentence.trim().split(" ");
+// let count = words.length;
+
+// console.log("Soz sayi:", count);
+
+
+
+
+
+
+
+
+
+// 3//
+// let name = "mURaD";
+
+// let formatted =
+//     name.charAt(0).toUpperCase() +
+//     name.slice(1).toLowerCase();
+
+// console.log(formatted);
+
+
+
+
+// 4//
+
+// let sentence = "Verilmis cumlede butun a herflerini evez et";
+
+// let result = sentence.replace(/a/g, "*");
+
+// console.log(result);
+
+
+
+
+
+
+
+// 5//
+// let email = "example@gmail.com";
+// let domain = email.spilt ("@") [1];
+// console.log("Domain",domain );
+
+
+
+
+
+
+
+// 6//
+// let sentence = "Javascript is really powerful language";
+
+// let words = sentence.split(" ");
+// let longest = "";
+
+// for (let word of words) {
+//     if (word.length > longest.length) {
+//         longest = word;
+//     }
+// }
+
+// console.log("Ən uzun söz:", longest);
